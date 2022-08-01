9d2384155c70cc2b45a1f5752ad1dc4f344a49f4
fulutas <ulutasfurkan@gmail.com>
2022-08-01 15:32:38 +0300

    tweet formats added and style changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ import {
   VerifiedIcon,
 } from './icons.js';
 
+// 1 regex - @ işaretinden sonraki tüm karakterleri span etiketine ekler. gi tüm tweet içerisindeki bulduğu herşeye uygulaması için.
+
+// 2 regex - # işaretinden a-z tüm harfler ve türkçe harflerde de uygulanması için kullanıldı.
+
+// 3 regex - http ve https ile başlayan linklere uygular.
+
+const tweetFormat = (tweet) => {
+  tweet = tweet
+    .replace(/@([\w]+)/gi, '<span>@$1</span>')
+    .replace(/#([\wşçöğüiıİ]+)/gi, '<span>#$1</span>')
+    .replace(/(https?:\/\/[\w\.\/]+)/gi, '<span>$1</span>');
+  return tweet;
+};
+
 export default function App() {
   const [name, setName] = useState();
   const [username, setUsername] = useState();
@@ -22,6 +36,35 @@ export default function App() {
     <>
       <div className="tweet-settings">
         <h3>Tweet Ayarları</h3>
+        <ul>
+          <li>
+            <input
+              type="text"
+              className="input"
+              placeholder="Ad Soyad"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </li>
+          <li>
+            <input
+              type="text"
+              className="input"
+              placeholdeR="Kullanıcı Adı"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </li>
+          <li>
+            <textarea
+              className="textarea"
+              maxLength="290"
+              placeholder="Tweet"
+              value={tweet}
+              onChange={(e) => setTweet(e.target.value)}
+            />
+          </li>
+        </ul>
       </div>
       <div className="tweet-container">
         <div className="tweet">
@@ -36,7 +79,13 @@ export default function App() {
             </div>
           </div>
           <div className="tweet-content">
-            <p>{tweet || 'Bu alana örnek tweet eklenecek.'}</p>
+            <p
+              dangerouslySetInnerHTML={{
+                __html:
+                  (tweet && tweetFormat(tweet)) ||
+                  'Bu alana örnek tweet eklenecek.',
+              }}
+            ></p>
           </div>
           <div className="tweet-stats">
             <span>
